d6ec7192652f8cfcf36876617e64d28527347c5c
J. Graves <jwgravesfl@gmail.com>
2018-09-29 16:11:44 -0600

    Routing page Main created

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import Main from './Main'
+
+const App = () => {
+  return (
+    <div>
+      <Main />
+    </div>
+  )
 }
 
-export default App;
+App.propTypes = {
+
+}
+
+export default App
+
