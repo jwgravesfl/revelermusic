3932f6a310905d53284c28446538ae46db43380f
J. Graves <jwgravesfl@gmail.com>
2018-09-29 16:58:41 -0600

    routing added, Page1(Home), Page2(LandingPage) and Page3(Contact) created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import Main from './Main'
+import Main from './navigation/Main'
 
 const App = () => {
   return (
---
+++ src/navigation/Main.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+import Page1 from '../components/Page1'
+import Page2 from '../components/Page2'
+import Page3 from '../components/Page3'
+
+export default class Main extends Component {
+  render() {
+    return (
+      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Page1} />
+          <Route path="/page2" component={Page2} />
+          <Route path="/page3" component={Page3} />
+        </Switch>
+      </BrowserRouter>
+    )
+  }
+}
