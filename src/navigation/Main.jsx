bbe4370fb5a1896fec1c185449b8611d6eee4328
J. Graves <jwgravesfl@gmail.com>
2018-09-29 20:25:46 -0600

    created NavBar Component

--- src/navigation/Main.jsx
+++ src/navigation/Main.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
+import NavBar from './NavBar'
 import Page1 from '../components/Page1'
 import Page2 from '../components/Page2'
 import Page3 from '../components/Page3'
@@ -11,6 +12,7 @@ export default class Main extends Component {
       
       <BrowserRouter>
         <Switch>
+        <NavBar />
           <Route exact path="/" component={Page1} />
           <Route path="/page2" component={Page2} />
           <Route path="/page3" component={Page3} />
