5343ebcdf58a5843c86b46a55c6796a55437094d
J. Graves <jwgravesfl@gmail.com>
2018-10-06 13:43:31 -0600

    page2 complete, add services, titles and descriptions, mobile complete

--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -22,6 +22,8 @@ const Page2Div = styled.div `
       background-image: url(${mobileBGimg});
     }
 
+    padding: 2vw;
+
     .colStyle {
       text-align: center;
       color: white;
@@ -30,10 +32,58 @@ const Page2Div = styled.div `
         width: 5vw;
       }
 
+      @media only screen and (max-width: 1200px) and (min-width: 700px)  {
+        padding-top: 2vh;
+
+        img {
+          width: 10vw;
+        }
+      }
+
+      @media only screen and (max-width: 700px) {
+        padding-top: 2vh;
+
+        img {
+          width: 23vw;
+        }
+      }
+
+    }
+
+    .servicesCard {
+      background: rgba(0, 0, 0, 0.8);
+      height: 100%;
+      
     }
 
     .servicesTitle {
+      font-family: 'Montserrat', sans-serif;
+      font-size: 2.5vw;
+      font-weight: 800;
+      font-variant: small-caps;
 
+      @media only screen and (max-width: 1200px) and (min-width: 700px)  {
+        font-size: 5vw;
+      }
+
+      @media only screen and (max-width: 700px) {
+        font-size: 10vw;
+      }
+    }
+
+    .servicesDescription {
+      font-family: 'PT Serif', serif;
+      font-size: .8vw;
+      font-weight: 600;
+
+      @media only screen and (max-width: 1200px) and (min-width: 700px)  {
+        font-size: 1.8vw;
+      }
+
+      @media only screen and (max-width: 700px) {
+        font-size: 3.5vw;
+      }
+      
     }
 
 `
@@ -43,47 +93,47 @@ const Page2 = () => {
   return (
     <Page2Div>
       <Row>
-        <Col className="colStyle">
-          <Card style={{ backgroundColor: "transparent" }} >
+        <Col className="colStyle" xl="3" md="6" xs="12">
+          <Card className="servicesCard" body outline color="secondary">
             <div >
-              <img top src={liveMusicImg} alt="Live Music icon" />
+              <img src={liveMusicImg} alt="Live Music icon" />
             </div>
             <CardBody>
-              <CardTitle>Live Music</CardTitle>
-              <CardText>With music brands to cover any event or occasion, Reveler Music offers Live Music services for Family Friendly or 21+ atmospheres.</CardText>
+              <CardTitle className="servicesTitle" >Live Music</CardTitle>
+              <CardText className="servicesDescription justify-content-center" >With music brands to cover any event or occasion, Reveler Music offers Live Music services for Family Friendly or 21+.</CardText>
             </CardBody>
           </Card>
         </Col>
-        <Col className="colStyle">
-          <Card inverse color="primary"  >
+        <Col className="colStyle" xl="3" md="6" xs="12">
+          <Card className="servicesCard" body outline color="secondary">
             <div >
-              <img src={openMicImg} alt="Open Mic icon" />
+             <img src={openMicImg} alt="Open Mic icon" />
             </div>
             <CardBody>
-              <CardTitle>Open Mics</CardTitle>
-              <CardText>Description</CardText>
+              <CardTitle className="servicesTitle" >Open Mics</CardTitle>
+              <CardText className="servicesDescription justify-content-center" >With professional modern sound equipment, Reveler Music offers talented Open Mic services for Music or Comedy events.</CardText>
             </CardBody>
           </Card>
         </Col>
-        <Col className="colStyle">
-          <Card inverse color="primary"  >
+        <Col className="colStyle" xl="3" md="6" xs="12">
+          <Card className="servicesCard" body outline color="secondary">
             <div >
-             <img src={karaokeImg} alt="mixer icon" />
+              <img src={karaokeImg} alt="mixer icon" />
             </div>
             <CardBody>
-              <CardTitle>Karaoke</CardTitle>
-              <CardText>With music 77,000+ songs for all genres, Reveler Music offers exciting karaoke services for occasion or event.</CardText>
+              <CardTitle className="servicesTitle" >Karaoke</CardTitle>
+              <CardText className="servicesDescription justify-content-center" >With 77,000+ songs of all skill level, Reveler Music offers entertaining and fun karaoke services for occasion, event or genre.</CardText>
             </CardBody>
           </Card>
         </Col>
-        <Col className="colStyle">
-          <Card inverse color="primary"  >
+        <Col className="colStyle" xl="3" md="6" xs="12">
+          <Card className="servicesCard" body outline color="secondary">
             <div >
               <img src={djImg} alt="mixer icon" />
             </div>
             <CardBody>
-              <CardTitle>DJ</CardTitle>
-              <CardText>Description</CardText>
+              <CardTitle className="servicesTitle" >DJ</CardTitle>
+              <CardText className="servicesDescription justify-content-center">With more than 30 years of music experience, Reveler Music offers spectacular dynamic DJ services to create the perfect experience.</CardText>
             </CardBody>
           </Card>
         </Col>
